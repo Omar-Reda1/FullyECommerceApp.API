822c5c414a49882ee7bf93d24288b431a11f147c
Omar-Reda1 <omar3000om30@gmail.com>
2025-12-05 17:48:28 +0200

    Add project files.

---
+++ FullyECommerceApp.API/Areas/Identity/Controllers/AccountsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FullyECommerceApp.API.Areas.Identity.Controllers
+{
+    [Route("api[area]/[controller]")]
+    [ApiController]
+    [Area("Identity")]
+    public class AccountsController : ControllerBase
+    {
+    }
+}
